a9cc97ce03253f8691410bc27a09728a2138566e
Potatoe37 <ggpth.dodo@gmail.com>
2020-06-16 14:15:57 +0200

    Changed evaluation funciton

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
 verb = True
-stop = False
+stop = True
 
 def vprint(s):
     if verb:
         print(s)
+
+def evalperf(c,processed,total_waiting_time,total_loss):
+    B = 0 #Cost of booking a packet
+    C = c #Cost of losing a packet
+    return (B*processed+C*total_loss+total_waiting_time)/(total_loss+processed)
